b7cfdbf1242dd753c0035b6378029ad6471b86be
Victor Turturika <v.turturika@gmail.com>
2024-06-09 23:29:43 +0300

    Add fething news articles from phm.cuspu.edu.ua

---
+++ data/fetch-article-links.R
@@ -0,0 +1,21 @@
+library(rvest)
+
+base_url <- "https://phm.cuspu.edu.ua/"
+base_news_url <- "https://phm.cuspu.edu.ua/facultet/novini.html"
+query_param <- "start"
+num_pages <- 10
+step <- 20
+file_path <- "article_links.txt"
+file_conn <- file(file_path, "w")
+
+for (i in 1:num_pages) {
+  cat("Fetching page", i, "of", num_pages, "\n")
+  
+  page_url <- paste0(base_news_url, "?", query_param, "=", (i-1)*step)
+  web_content <- read_html(page_url)
+  links <- html_attr(html_nodes(web_content, css = ".page-header a"), "href")
+  full_links <- paste(base_url, links, sep = "")
+  writeLines(full_links, file_conn)
+}
+
+close(file_conn)
---
+++ data/fetch-article-texts.R
@@ -0,0 +1,43 @@
+library(rvest)
+library(stringr)
+
+input_file <- "article_links.txt"
+output_file <- "article_texts.csv"
+
+article_links <- readLines(input_file)
+
+start <- 1
+end <- length(article_links)
+
+
+results <-
+  data.frame(link = character(),
+             title = character(),
+             text = character(),
+             stringsAsFactors = FALSE)
+
+if (file.exists(output_file)) {
+  existing_data <- read.csv(output_file, stringsAsFactors = FALSE)
+  results <- rbind(results, existing_data)
+}
+
+
+for (i in seq(start, end)) {
+  link <- article_links[i]
+
+  cat("Fetching link", i, "of", end, ":", link, "\n")
+  
+  web_content <- read_html(link)
+  title <- html_text2(html_node(web_content, css = ".page-header h1"))
+  text <- html_text2(html_node(web_content, css = ".article__text"))
+  text <- str_squish(text)
+  
+  results <- rbind(results, data.frame(link = link,
+                                       title = title,
+                                       text = text,
+                                       stringsAsFactors = FALSE))
+
+  cat("Successfully fetched and stored text from link", i, "\n")
+}
+
+write.csv(results, output_file, row.names = FALSE)
